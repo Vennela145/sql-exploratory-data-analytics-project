46328c2f0dd63efe53099813fe6e7e60187adaf0
Vennela145 <salvajivennela@gmail.com>
2026-07-16 00:44:59 +0530

    Add files via upload

---
+++ scripts/changes_over_time_analysis.sql
@@ -0,0 +1,31 @@
+--Analyze Sales Performance Over Time
+SELECT 
+MONTH(order_date) AS order_month,
+YEAR(order_date) AS order_year,
+SUM(sales_amount) as total_sales,
+COUNT(DISTINCT customer_key) as total_customers,
+SUM(quantity) as total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY YEAR(order_date), MONTH(order_date)
+ORDER BY YEAR(order_date),MONTH(order_date)
+
+SELECT
+DATETRUNC(year,order_date) as order_year,
+SUM(sales_amount) as total_sales,
+COUNT(DISTINCT customer_key) as total_customers,
+SUM(quantity) as total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY DATETRUNC(year,order_date)
+ORDER BY DATETRUNC(year,order_date)
+
+SELECT
+FORMAT(order_date, 'yyyy-MMM') as order_year,
+SUM(sales_amount) as total_sales,
+COUNT(DISTINCT customer_key) as total_customers,
+SUM(quantity) as total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY FORMAT(order_date, 'yyyy-MMM')
+ORDER BY FORMAT(order_date, 'yyyy-MMM')
